28a3e82df75423b7d6fb68afa38503174e6cbfe6
amanwrites01 <aman143ko@gmail.com>
2025-06-05 10:33:16 +0530

    i learned Function

---
+++ hello1.js
@@ -0,0 +1,10 @@
+// const prompt = require('prompt-sync')();
+//const name = prompt("What is your name? ");
+//console.log("Hello, " + name + "!");
+
+function sq(x){
+    let ans=0;
+    ans=x*x;
+    return ans;
+}
+console.log("square of the number:" +sq(7));
