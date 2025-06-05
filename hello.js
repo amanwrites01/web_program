0411ad75cbbcb4a69eba9ee4d98a000b1210fd1e
amanwrites01 <aman143ko@gmail.com>
2025-06-05 09:27:21 +0530

    changes

---
+++ hello.js
@@ -0,0 +1,37 @@
+console.log("Helloo World");
+const pi="3.14";
+console.log(pi)
+let num=21;
+if(num %2==0)
+{
+    console.log("Num is even")
+}
+else{
+    console.log("Num is odd")
+}
+let day=3;
+let dayName;
+switch(day)
+{
+    case 1:
+        dayName="Monday";
+        break;
+    case 2:
+        dayName="Tuesday";
+        break;
+    case 3:
+        dayName="Wednesday";
+        break;
+    case 4:
+        dayName="Thursday";
+        break;
+    case 5:
+        dayName="Friday";
+        break;
+    case 6:
+        dayName="Saturday";
+        break;
+    case 7:
+        dayName="Sunday";
+        break;
+}
